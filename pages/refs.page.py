c913516a0d8393a914b54ef1a44272225f8b6a98
Danilo Henrique Achcar <dnl_pnd16@hotmail.com>
2024-02-14 17:07:18 -0300

    feat(assets): adicionado imgs do google-bigquery e google-cloud-storage

--- pages/refs.page.py
+++ pages/refs.page.py
@@ -8,8 +8,10 @@ with st.container():
     st.header('Referências')
 
     st.markdown('''
-    - O que é. PNAD COVID19. IBGE. https://www.ibge.gov.br/estatisticas/investigacoes-experimentais/estatisticas-experimentais/27946-divulgacao-semanal-pnadcovid1?t=o-que-e\n\n
-    - Microdados. PNAD COVID19. IBGE. https://www.ibge.gov.br/estatisticas/investigacoes-experimentais/estatisticas-experimentais/27946-divulgacao-semanal-pnadcovid1?t=microdados\n\n
-    - Informações Técnicas. PNAD COVID19. IBGE. https://www.ibge.gov.br/estatisticas/investigacoes-experimentais/estatisticas-experimentais/27946-divulgacao-semanal-pnadcovid1?t=notas-tecnicas\n\n
-    - Apache Spark. Apache. https://spark.apache.org/
+    - O que é. PNAD COVID19. IBGE. Disponível em: https://www.ibge.gov.br/estatisticas/investigacoes-experimentais/estatisticas-experimentais/27946-divulgacao-semanal-pnadcovid1?t=o-que-e\n\n
+    - Microdados. PNAD COVID19. IBGE. Disponível em: https://www.ibge.gov.br/estatisticas/investigacoes-experimentais/estatisticas-experimentais/27946-divulgacao-semanal-pnadcovid1?t=microdados\n\n
+    - Informações Técnicas. PNAD COVID19. IBGE. Disponível em: https://www.ibge.gov.br/estatisticas/investigacoes-experimentais/estatisticas-experimentais/27946-divulgacao-semanal-pnadcovid1?t=notas-tecnicas\n\n
+    - Apache Spark. Apache. Disponível em: https://spark.apache.org/
+    - Google Big Query. Google. Disponível em: https://cloud.google.com/bigquery
+    - Google Cloud Storage. Google. Disponível em: https://cloud.google.com/storage
     ''')
--- tabs/intro/google_bigquery_tab.py
+++ tabs/intro/google_bigquery_tab.py
@@ -10,6 +10,18 @@ class IntroGoogleBigQueryTab(TabInterface):
         with self.tab:
             st.subheader(':blue[Google BigQuery]', divider='blue')
             st.markdown('''
+                O Google BigQuery é um serviço de armazenamento e análise de dados na nuvem oferecido pelo Google. Ele permite que as organizações armazenem, consultem e analisem grandes volumes de dados de maneira rápida e eficiente, usando a linguagem de consulta SQL padrão. O BigQuery é altamente escalável, oferecendo capacidade de processamento sob demanda e integração fácil com outras ferramentas e serviços do ecossistema do Google Cloud Platform (GCP).\n\n
+                O BigQuery é o serviço em nuvem escolhido para a realização deste trabalho.
+
                 TODO: explicar oq é e como estou utilizando... colocar prints das tabelas criadas da PNAD\n\n
                 gerar tabelas com as colunas (no bigquery, explicar que serão utilizadas 2 tipos de tabela, inicial e processado)
+            ''')
+
+            st.subheader(':blue[Tabelas]', divider='blue')
+            st.markdown('''
+                Dentro do BigQuery, temos 2 tipos de tabela:
+                1) as tabelas com os dados originais;
+                2) as tabelas processadas, contemplando exclusivamente as colunas/features que serão utilizadas durante o projeto;
+                        
+                As tabelas com os dados originais foram geradas a partir de arquivos CSV que foram enviados ao Google Cloud Storage, um serviço de armazenamento em nuvem (muito parecido com o S3 da Amazon).
             ''')
